83b32a2c6954e07f9c92011e00c091934166d42f
John Snow <john.snow@email.com>
2026-01-29 01:45:41 +0100

    Initial Release

---
+++ index.js
@@ -0,0 +1,14 @@
+let myObj = {
+       prop1: 'val1',
+prop2: 'val2',
+}
+
+function min(x, y) { if (x < y) {
+    return x
+} else {
+return y
+}
+}
+let x = min(1, 3);
+  console.log("x="+x)
+console.log(myObj)
