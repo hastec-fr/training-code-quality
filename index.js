9d141123a58822c17e9bcf4b9b124e793991a5ab
John Snow <john.snow@email.com>
2026-01-29 01:50:33 +0100

    First Prettier format

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let myObj = {
-       prop1: 'val1',
-prop2: 'val2',
-}
+  prop1: "val1",
+  prop2: "val2",
+};
 
-function min(x, y) { if (x < y) {
-    return x
-} else {
-return y
-}
+function min(x, y) {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }
 }
 let x = min(1, 3);
-  console.log("x="+x)
-console.log(myObj)
+console.log("x=" + x);
+console.log(myObj);
